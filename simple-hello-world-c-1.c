dbd5c870f7819427c15453a6efffeac0e74ed27b
cupboardmanufacturer <160159901+cupboardmanufacturer@users.noreply.github.com>
2024-05-17 08:57:21 -0700

    Create simple-hello-world-c-1.c
    a simple hello world in c 100% trust me not complicated at all

---
+++ simple-hello-world-c-1.c
@@ -0,0 +1,51 @@
+#define e int
+#define ee main
+#define eee printf
+#define eeee "H"
+#define eeeee "e"
+#define eeeeee "l"
+#define eeeeeee "l"
+#define eeeeeeee "o"
+#define eeeeeeeee ","
+#define eeeeeeeeee " "
+#define eeeeeeeeeee "W"
+#define eeeeeeeeeeee "o"
+#define eeeeeeeeeeeee "r"
+#define eeeeeeeeeeeeee "l"
+#define eeeeeeeeeeeeeee "d"
+#define eeeeeeeeeeeeeeee "!"
+#define eeeeeeeeeeeeeeeee "\n"
+#define eeeeeeeeeeeeeeeeee return
+#define eeeeeeeeeeeeeeeeeee 0
+#define eeeeeeeeeeeeeeeeeeee printf
+#define eeeeeeeeeeeeeeeeeeeee printf
+#define eeeeeeeeeeeeeeeeeeeeee printf
+#define eeeeeeeeeeeeeeeeeeeeeee printf
+#define eeeeeeeeeeeeeeeeeeeeeeee printf
+#define eeeeeeeeeeeeeeeeeeeeeeeee printf
+#define eeeeeeeeeeeeeeeeeeeeeeeeee printf
+#define eeeeeeeeeeeeeeeeeeeeeeeeeee printf
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeee printf
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeeee printf
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeeeee printf
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee printf
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee printf
+
+
+e ee() {
+    eee(eeee);
+    eeeeeeeeeeeeeeeeeeee(eeeee);
+    eeeeeeeeeeeeeeeeeeeee(eeeeee);
+    eeeeeeeeeeeeeeeeeeeeee(eeeeeee);
+    eeeeeeeeeeeeeeeeeeeeeee(eeeeeeee);
+    eeeeeeeeeeeeeeeeeeeeeeee(eeeeeeeee);
+    eeeeeeeeeeeeeeeeeeeeeeeee(eeeeeeeeee);
+    eeeeeeeeeeeeeeeeeeeeeeeeee(eeeeeeeeeee);
+    eeeeeeeeeeeeeeeeeeeeeeeeeee(eeeeeeeeeeee);
+    eeeeeeeeeeeeeeeeeeeeeeeeeeee(eeeeeeeeeeeee);
+    eeeeeeeeeeeeeeeeeeeeeeeeeeeee(eeeeeeeeeeeeee);
+    eeeeeeeeeeeeeeeeeeeeeeeeeeeeee(eeeeeeeeeeeeeee);
+    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee(eeeeeeeeeeeeeeee);
+    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee(eeeeeeeeeeeeeeeee);
+    eeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeee;
+}
